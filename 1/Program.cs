e147bd1bdfb6465f09fd7e84f873940ed5aa70e8
Denis <denis989222@gmail.com>
2023-12-03 17:56:52 +0300

    NewArrayOfStrings

---
+++ 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с 
+// клавиатуры, либо задать на старте выполнения алгоритма. При 
+// решении не рекомендуется пользоваться коллекциями, лучше 
+// обойтись исключительно массивами.
+
+
+string [] array = {"1234", "1567", "-2", "computer science", "1", " ", "111", "1111"};
+
+void PrintArray(string[] array)
+{
+    Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        Console.Write($", {array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void NewArrayOfStrings(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<4)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+}
+
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+NewArrayOfStrings(array);
